b3c7d58d8c5f888cdc0549deed916df20fa1c8ef
AmanPurohit73 <amanpurohit315@gmail.com>
2025-04-17 00:35:57 +0530

    auth done

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,19 @@
 import Header from "@/components/Header";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Journal",
+  title: "NoteFul",
   description: "A Journal app",
 };
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
+
     <html lang="en">
       <body className={inter.className}>
         <div className="fixed inset-0 -z-10 bg-[url('/bg.jpg')] opacity-50" />
@@ -18,5 +21,6 @@ export default function RootLayout({ children }) {
         <main className="min-h-screen">{children}</main>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,21 +1,55 @@
 import Link from "next/link";
 import React from "react";
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import { FolderOpen, PenBox } from "lucide-react";
+import UserMenu from "./UserMenu";
 
 const Header = () => {
   return (
     <header className="container mx-auto">
-      <nav className="py-6 px-4 flex justiy-between items-center">
+      <nav className="py-6 px-4 flex justify-between items-center">
         <Link href={"/"}>
           <img
             src={"/logo.svg"}
             width={200}
             height={60}
-            className="h-10 w-auto object-contain"
+            className="h-10 w-auto object-contain pl-10"
           />
         </Link>
 
         <div className="flex items-center gap-4">
-            {}
+          <SignedIn>
+            <Link href="/dashboard#collections">
+              <Button variant="outline" className="flex items-center gap-2">
+                <FolderOpen size={18} />
+                <span className="hidden md:inline">Collections</span>
+              </Button>
+            </Link>
+          </SignedIn>
+
+          <Link href="/journal/write">
+            <Button variant="journal" className="flex items-center gap-2">
+              <PenBox size={18} />
+              <span className="hidden md:inline">Write New</span>
+            </Button>
+          </Link>
+
+          <SignedOut>
+            <SignInButton forceRedirectUrl="/dashboard">
+              <Button variant="outline">Login</Button>
+            </SignInButton>
+          </SignedOut>
+
+          <SignedIn>
+            <UserMenu/>
+          </SignedIn>
         </div>
       </nav>
     </header>
